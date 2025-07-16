72bcc2b855c9686563b803ae30725c67db13ec0f
ANBU-2710 <anbuselvamv27@gmail.com>
2025-07-16 18:53:41 +0530

    Update logic_gates.v

--- verilog/Gatelevel/day1/logic_gates.v
+++ verilog/Gatelevel/day1/logic_gates.v
@@ -1,1 +1,20 @@
-
+module logic_gates(a,b,c,y);
+  //declare input & output pins
+  input a,b,c;
+  output y;
+  //here verilog code by gateprimitives for logic_gates
+  and(y,a,b);
+  or(y,a,b);
+  xor(y,a,b);
+  nand(y,a,b);
+  nor(y,a,b);
+  xnor(y,a,b);
+  //buffers
+  buf(y,a);
+  bufif1(y,a,b); //here 'b' as a control signal also for not logic 
+  bufif0(y,a,b);
+  //not logic
+  not(y,a);
+  notif1(y,a,b);
+  notif0(y,a,b);
+endmodule

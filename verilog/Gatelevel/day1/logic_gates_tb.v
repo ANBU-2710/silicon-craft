39c3d94a7586891ca117b847e1fde16a2152f4b1
ANBU-2710 <anbuselvamv27@gmail.com>
2025-07-16 23:10:35 +0530

    Update logic_gates_tb.v

--- verilog/Gatelevel/day1/logic_gates_tb.v
+++ verilog/Gatelevel/day1/logic_gates_tb.v
@@ -6,9 +6,19 @@ module logic_gates_tb;
   //instantiate the logicgate module
   logic_gates gates(
     .a(a),
-    .b(b),
+    .b(b), 
     .y(y)
   );
   //open initial block
   initial begin
-    $
+    $monitor("Time=%0t,a= %b : b= %b --> y=%b",$time,a,b,y);
+    a=0;b=0;
+    #10;
+    a=0;b=1;
+    #10;
+    a=1;b=0;
+    #10;
+    a=1,b=1;
+    $finish;
+  end
+endmodule

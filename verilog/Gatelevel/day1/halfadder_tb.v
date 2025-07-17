ff2b7f5902fa0bced3ba31c128752fd7582cf505
ANBU-2710 <anbuselvamv27@gmail.com>
2025-07-17 13:04:10 +0530

    Update halfadder_tb.v

--- verilog/Gatelevel/day1/halfadder_tb.v
+++ verilog/Gatelevel/day1/halfadder_tb.v
@@ -1,26 +1,28 @@
 module halfadder_tb;
-reg a,b;
-wire s,c;
-halfadder h_add(
-.a(a),
-.b(b),
-.sum(s),
-.cout(c)
-);
-//use $dumpfile,$dumpvars to see waveform in gtkwave
-initial begin
-$dumpfile("h_add.vcd");
-$dumpvars(0,halfadder_tb);
-end
-initial begin
-$monitor("Time=%0t\t a=%b|b=%b||s=%b|c=%b",$time,a,b,s,c);
-a=0;b=0;
-#10;
-a=0;b=1;
-#10;
-a=1;b=0;
-#10;
-a=1;b=1;
-$finish;
-end
+  reg a,b;
+  wire s,c;
+  //instantiate the design module to testbench
+  halfadder h_add(
+    .a(a),
+    .b(b),
+    .sum(s),
+    .cout(c)
+    );
+  //use $dumpfile,$dumpvars to see waveform in gtkwave
+  //dumping the values
+  initial begin
+    $dumpfile("h_add.vcd");
+    $dumpvars(0,halfadder_tb);
+  end
+  initial begin
+    $monitor("Time=%0t\t a=%b|b=%b||s=%b|c=%b",$time,a,b,s,c);
+    a=0;b=0;
+    #10;
+    a=0;b=1;
+    #10;
+    a=1;b=0;
+    #10;
+    a=1;b=1;
+    $finish;
+  end
 endmodule

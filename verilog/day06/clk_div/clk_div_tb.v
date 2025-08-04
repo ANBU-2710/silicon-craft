3e36d0931ee8c25375cc0fede6db8d749e8d1cf3
ANBUSELVAM V <anbuselvamv27@gmail.com>
2025-08-04 12:40:32 +0530

    Update clk_div_tb.v

--- verilog/day06/clk_div/clk_div_tb.v
+++ verilog/day06/clk_div/clk_div_tb.v
@@ -1,2 +1,24 @@
-module clk_div_tb;
+module clk_1hz_tb;
+reg clk,rst;
+wire clk_out;
+clk_div_1hz clk1hz(
+        .clk(clk),
+        .rst(rst),
+        .clk_out(clk_out)
+);
+initial begin
+        $dumpfile("clk_10.vcd");
+        $dumpvars(0,clk_1hz_tb);
+end
+initial clk = 0;
+always #5 clk = ~clk;
+initial begin
+        $monitor("clk=%b|rst=1||clk_out=%b",clk,rst,clk_out);
+        rst =0;
+        #10;
+        rst=1;
+        #500;
+        $finish;
+end
+    
 endmodule
